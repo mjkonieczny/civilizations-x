efdaa3703680409fe102c1dab1f23ab3fef56cb7
Mateusz Konieczny <matthew.the.equine@gmail.com>
2022-11-07 13:34:44 +0100

    create board limitations

--- src/commands/createBoard.ts
+++ src/commands/createBoard.ts
@@ -1,16 +1,31 @@
 import { Command } from '../model'
 
-export const createBoardCommandFactory = (n: number, m: number): Command => (game) => ({
-  ...game,
-  board: {
-    n,
-    m
-  },
-  logs: [
-    ...game.logs,
-    {
-      text: `Board created with ${n} rows and ${m} columns`,
-      level: 'info'
+export const createBoardCommandFactory = (n: number, m: number): Command => (game) => {
+  if (n < 1 || n > 250 || m < 1 || m > 250) {
+    return {
+      ...game,
+      logs: [
+        ...game.logs,
+        {
+          text: 'Board size must be between 1 and 250',
+          level: 'error'
+        }
+      ]
     }
-  ]
-})
+  }
+
+  return {
+    ...game,
+    board: {
+      n,
+      m,
+    },
+    logs: [
+      ...game.logs,
+      {
+        text: `Board created with ${n} rows and ${m} columns`,
+        level: 'info'
+      }
+    ]
+  }
+}
--- src/parser/input.test.ts
+++ src/parser/input.test.ts
@@ -22,10 +22,13 @@ describe('input should be parsed', () => {
     ])
   })
 
-  it('should create board', () => {
+  it.each([
+    ['create board 1 1', 1, 1, 'Board created with 1 rows and 1 columns', 'info'],
+    ['create board 3 4', 3, 4, 'Board created with 3 rows and 4 columns', 'info'],
+  ])('should %s', (command, n, m, text, level) => {
     // given
     const commands = parse(`
-      create board 1 2
+      ${command}
     `)
 
     // when
@@ -34,13 +37,43 @@ describe('input should be parsed', () => {
     // then
     expect(result).toEqual({
       board: {
-        n: 1,
-        m: 2
+        n,
+        m,
       },
       logs: [
         {
-          text: 'Board created with 1 rows and 2 columns',
-          level: 'info'
+          text,
+          level
+        }
+      ]
+    })
+  })
+
+  it.each([
+    ['create board 0 1', 'Board size must be between 1 and 250', 'error'],
+    ['create board 1 0', 'Board size must be between 1 and 250', 'error'],
+    ['create board 251 1', 'Board size must be between 1 and 250', 'error'],
+    ['create board 1 251', 'Board size must be between 1 and 250', 'error'],
+  ])('should not %s', (command, text, level) => {
+    // given
+    const commands = parse(`
+
+      ${command}
+    `)
+
+    // when
+    const result = execute(commands)
+
+    // then
+    expect(result).toEqual({
+      board: {
+        n: 0,
+        m: 0,
+      },
+      logs: [
+        {
+          text,
+          level
         }
       ]
     })
